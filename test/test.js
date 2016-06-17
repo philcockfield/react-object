aca3aa0c81c34c3769d3b4f5a98eb3a8cd1d3d7d
Phil Cockfield <phil@cockfield.net>
2016-06-17 21:28:29 +1200

    Update NPM refs.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,2 @@
-"use strict";
-import { expect } from "chai";
-
-describe("react-object tests", () => {
-  it("succeeds", () => {
-    expect(123).to.equal(123);
-  });
-});
+'use strict';
+import { expect } from 'chai';
